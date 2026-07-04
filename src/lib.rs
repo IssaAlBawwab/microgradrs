35ccc73b18441398f4e832ac305e6ed227d1e8af
Issa AlBawwab <issa.albawwab.yes@gmail.com>
2026-07-04 13:43:41 +0300

    wrap actual value around Value struct which allows each value to have its own Rc at initialization time, allow addition without moving values

--- src/lib.rs
+++ src/lib.rs
@@ -2,35 +2,46 @@ use std::cell::{Ref, RefCell};
 use std::ops::Add;
 use std::rc::Rc;
 #[derive(Debug)]
-pub struct Value {
+pub struct ValueData {
     pub data: f32,
     pub op: Operation,
     pub gradient: f32,
-    pub parents: Vec<Rc<RefCell<Value>>>,
+    pub parents: Vec<Value>,
 }
+
+#[derive(Debug, Clone)]
+pub struct Value(Rc<RefCell<ValueData>>);
 
 impl Value {
     //pub fn backward(dyn  ) ->
     pub fn new(data: f32) -> Value {
-        Self {
+        Value(Rc::new(RefCell::new(ValueData {
             data,
             op: Operation::None,
             gradient: 0.0,
             parents: vec![],
-        }
+        })))
+    }
+    pub fn data(&self) -> f32 {
+        self.0.borrow().data
+    }
+    pub fn gradient(&self) -> f32 {
+        self.0.borrow().gradient
     }
 }
-impl Add for Value {
-    type Output = Self;
-    fn add(self, other: Self) -> Self::Output {
-        Self {
-            data: self.data + other.data,
+impl<'a, 'b> Add<&'b Value> for &'a Value {
+    type Output = Value;
+    fn add(self, other: &'b Value) -> Value {
+        let data = self.data() + other.data();
+        Value(Rc::new(RefCell::new(ValueData {
+            data,
             op: Operation::Add,
             gradient: 0.0,
-            parents: vec![Rc::new(RefCell::new(self)), Rc::new(RefCell::new(other))],
-        }
+            parents: vec![self.clone(), other.clone()],
+        })))
     }
 }
+
 #[derive(Debug)]
 pub enum Operation {
     Add,
@@ -49,6 +60,6 @@ mod tests {
         let mut b = Value::new(2.0);
         let mut c = a + b;
 
-        assert_eq!(c.data, 3.0);
+        assert_eq!(c.data(), 3.0);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@ use microgradrs::{Operation, Value};
 
 fn main() {
     let mut a = Value::new(1.0);
-    println!("{:?}", a);
     let mut b = Value::new(2.0);
-    let mut c = a + b;
-    print!("{:?}", c);
+    let mut c = &a + &b;
+    let d = &(&a + &b) + &c;
+    println!("{:?}", d)
 }
